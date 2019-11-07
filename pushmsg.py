97facf55e21f9488f5ded05fb42750a592266b66
Marek Kuczynski <marek.kuczynski@gmail.com>
2019-11-07 15:20:16 +0100

    adding publish python

---
+++ pushmsg.py
@@ -0,0 +1,19 @@
+import boto3, sys, time
+
+ddb     = boto3.client('dynamodb')
+ts      = str(int(time.time()))
+user    = 'marek'
+msg	= sys.argv[1]
+
+
+def push():
+    ddb.put_item(TableName = 'geochat', 
+	Item = {
+	    'timest'	: {"S": ts } ,
+	    'message'	: {"S": msg},
+            'user'      : {"S": user }
+	})
+
+    print('added item '+msg+' from user '+user)
+
+push()
